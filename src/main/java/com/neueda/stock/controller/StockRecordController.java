d8c09c773122769ca6e4ebb41000395192a12039
Yuxi S <1209944541@qq.com>
2023-08-14 09:15:44 +0000

    create controller folder

---
+++ src/main/java/com/neueda/stock/controller/StockRecordController.java
@@ -0,0 +1,13 @@
+package com.neueda.stock.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockRecordController {
+	//create Logger
+	Logger logger= LoggerFactory.getLogger(StockRecordController.class);
+	
+	
+}
