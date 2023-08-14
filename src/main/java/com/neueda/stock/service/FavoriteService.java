0f946a4128e23aef0baa5a87fa2ba86ab22f5d58
Your Name <you@example.com>
2023-08-14 07:07:00 +0000

    update

--- src/main/java/com/neueda/stock/service/FavoriteService.java
+++ src/main/java/com/neueda/stock/service/FavoriteService.java
@@ -1,28 +1,46 @@
 package com.neueda.stock.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.neueda.stock.entity.FavoriteRecord;
+import com.neueda.stock.repo.FavoriteRecordRepo;
 
 public class FavoriteService {
+	
+	@Autowired
+	private FavoriteRecordRepo repo;
 	
 	/*add a favorited stock record
 	 * return all stock record
 	 * */
-	public List<FavoriteRecord>add(String stocksymbol,String date) {
-		return;
+	public List<FavoriteRecord>add(FavoriteRecord frecord) {
+		repo.save(frecord);// add the new favorite record
+		return getAll();// return all the favorite records after add
 	}
 	
 	//get all favorited stock record
 	public List<FavoriteRecord> getAll(){
-		return ;
+		return repo.findAll();
 	}
 	
 	//count  favorited stock record
 	public int getCount() {
-		return ;
+		return (int)repo.count();
 	}
 	
-	//delete a favorited stock record by stock symbol
-	public List<FavoriteRecord> delete(String stocksymbol){
+	/*delete a favorited stock record
+	 * return all stock record
+	 * */
+	public List<FavoriteRecord> delete(int id){
+		if(repo.findById(id).isPresent()) { // whether the delete favorited record is exist
+			repo.deleteById(id);// if it exists, delete it
+			 return repo.findAll();//
+		}
+		else {
+			return null;
+		}
 		
 	}
 	
