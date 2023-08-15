448825370e84c1468d335699c7ada24a2e172c59
Your Name <you@example.com>
2023-08-15 03:56:02 +0000

    update

--- src/main/java/com/neueda/stock/controller/FavoriteController.java
+++ src/main/java/com/neueda/stock/controller/FavoriteController.java
@@ -1,13 +1,75 @@
 package com.neueda.stock.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.neueda.stock.entity.FavoriteRecord;
+import com.neueda.stock.service.FavoriteService;
+@CrossOrigin("http://localhost:8080/")
 @RestController
+@RequestMapping("/api/favoriteRecord")
 public class FavoriteController {
 	//create Logger
 	Logger logger= LoggerFactory.getLogger(FavoriteController.class);
+	
+	@Autowired
+	private FavoriteService favoriteService;
+	
+	//post request
+	@PostMapping("/")
+	public ResponseEntity<FavoriteRecord> addFavoriteRecord(@RequestBody FavoriteRecord fRecord){		
+		FavoriteRecord favoriteRecord= favoriteService.add(fRecord);		
+		if(favoriteRecord!=null)
+			return new ResponseEntity<FavoriteRecord>(favoriteRecord,HttpStatus.CREATED);
+		else
+			return new ResponseEntity<FavoriteRecord>(favoriteRecord,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	/*get request
+	 * get all favorited records
+	 *  */  
+	@GetMapping("/")
+	public List<FavoriteRecord> getAllFavoriteRecord(){
+		return favoriteService.getAll();
+	}
+	
+	/*get request
+	 * get the count of  favorited records
+	 *  */ 
+	@GetMapping("/count/")
+	public ResponseEntity<Integer>getCount (){
+		int count= favoriteService.getCount();
+		if(count>0)
+			return new ResponseEntity<Integer>(count, HttpStatus.OK);
+		else
+			return new ResponseEntity<Integer>(count, HttpStatus.NOT_FOUND);
+	}
+	
+	/*delete request
+	 * delete a favorited records by stocksymbol
+	 *  */
+	@DeleteMapping("/{stocksymbol}")
+	public ResponseEntity<String>deleteFavoriteRecord(@PathVariable String stocksymbol){
+		boolean result = favoriteService.delete(stocksymbol);
+		if(result) 
+			return new ResponseEntity<String>("Object Deleted",HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("NO user Found", HttpStatus.NOT_FOUND);		
+	}
 	
 	
 }
--- src/main/java/com/neueda/stock/service/FavoriteService.java
+++ src/main/java/com/neueda/stock/service/FavoriteService.java
@@ -3,21 +3,20 @@ package com.neueda.stock.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.neueda.stock.entity.FavoriteRecord;
 import com.neueda.stock.repo.FavoriteRecordRepo;
 
+@Service
 public class FavoriteService {
 	
 	@Autowired
 	private FavoriteRecordRepo repo;
 	
-	/*add a favorited stock record
-	 * return all stock record
-	 * */
-	public List<FavoriteRecord>add(FavoriteRecord frecord) {
-		repo.save(frecord);// add the new favorite record
-		return getAll();// return all the favorite records after add
+	//*add a favorited stock record
+	public FavoriteRecord add(FavoriteRecord frecord) {
+		return repo.save(frecord);// add the new favorite record
 	}
 	
 	//get all favorited stock record
@@ -30,16 +29,16 @@ public class FavoriteService {
 		return (int)repo.count();
 	}
 	
-	/*delete a favorited stock record
+	/*delete a favorited stock record by
 	 * return all stock record
 	 * */
-	public List<FavoriteRecord> delete(String id){
-		if(repo.findById(id).isPresent()) { // whether the delete favorited record is exist
-			repo.deleteById(id);// if it exists, delete it
-			 return repo.findAll();//
+	public boolean delete(String stocksymbol){
+		if(repo.findById(stocksymbol).isPresent()) { // whether the delete favorited record is exist
+			repo.deleteById(stocksymbol);// if it exists, delete it
+			return true;
 		}
 		else {
-			return null;
+			return false;
 		}
 		
 	}
